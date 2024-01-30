ebfe3be5d3766661ed73c16a5dad26df2385fa87
Jhems <Er.jhems@gmail.com>
2024-01-30 19:19:10 +0530

    Time Picker

--- TimePicker/app/src/main/java/com/jhems/timepicker/MainActivity.java
+++ TimePicker/app/src/main/java/com/jhems/timepicker/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jhems.timepicker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showTimePickerDialog(View view) {
+        final Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                (view1, hourOfDay, minute1) -> {
+                    String selectedTime = hourOfDay + ":" + minute1;
+                    Toast.makeText(MainActivity.this, "Selected Time: " + selectedTime, Toast.LENGTH_SHORT).show();
+                }, hour, minute, true);
+        timePickerDialog.show();
     }
 }
